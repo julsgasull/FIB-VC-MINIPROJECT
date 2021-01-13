d6048abdc7f2f5bb2e1e5158f4eae7aae7bbf07b
juls <julia.gasull.i@est.fib.upc.edu>
2021-01-13 17:54:33 +0100

    yeeey

--- processar_imatge.m
+++ processar_imatge.m
@@ -10,46 +10,26 @@ function features = processar_imatge(image)
 
     blockSizeR = rows/2;           % Rows in block.
     blockSizeC = columns/2;     % Columns in block.
-
+    
     % Figure out the size of each block in rows.
-    % Most will be blockSizeR but there may be a remainder amount of less than that.
     wholeBlockRows = floor(rows / blockSizeR);
-    blockVectorR = [blockSizeR * ones(1, wholeBlockRows), rem(rows, blockSizeR)];
+    blockVectorR = [blockSizeR * ones(1, wholeBlockRows)];
+    
     % Figure out the size of each block in columns.
     wholeBlockCols = floor(columns / blockSizeC);
-    blockVectorC = [blockSizeC * ones(1, wholeBlockCols), rem(columns, blockSizeC)];
-
+    blockVectorC = [blockSizeC * ones(1, wholeBlockCols)];
+    
     % Create the cell array, ca. 
-    % Each cell (except for the remainder cells at the end of the image)
-    % in the array contains a blockSizeR by blockSizeC by 3 color array.
-    % This line is where the image is actually divided up into blocks.
     ca = mat2cell(image, blockVectorR, blockVectorC, numberOfColorBands);
-
+    
     % Now display all the blocks.
-    plotIndex = 1;
-    numPlotsR = size(ca, 1);
-    numPlotsC = size(ca, 2);
+    numPlotsR = size(ca, 1); numPlotsC = size(ca, 2); 
     for r = 1 : numPlotsR
         for c = 1 : numPlotsC
-            fprintf('plotindex = %d,   c=%d, r=%d\n', plotIndex, c, r);
-            % Specify the location for display of the image.
-            subplot(numPlotsR, numPlotsC, plotIndex);
-            % Extract the numerical array out of the cell
-            % just for tutorial purposes.
+            % Extract the numerical array out of the cell just for tutorial purposes.
             rgbBlock = ca{r,c};
             [histRed, histBlue] = colorHistogramFunction(rgbBlock);
             features =  [features, histRed, histBlue];
-            
-            imshow(rgbBlock); % Could call imshow(ca{r,c}) if you wanted to.
-            [rowsB, columnsB, numberOfColorBandsB] = size(rgbBlock);
-            % Make the caption the block number.
-            caption = sprintf('Block #%d of %d\n%d rows by %d columns', ...
-            plotIndex, numPlotsR*numPlotsC, rowsB, columnsB);
-            title(caption);
-            drawnow;
-            
-            % Increment the subplot to the next location.
-            plotIndex = plotIndex + 1;
         end
     end
 
